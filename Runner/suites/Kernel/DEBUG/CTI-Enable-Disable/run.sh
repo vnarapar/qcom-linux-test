Rohan-in-Qualcomm/qcom-linux-testkit

Added CTI enable/disable, trigger mapping, and sysfs node access tests
This commit adds CTI validation test cases to verify basic enable/disable operations, trigger channel mapping functionality, and sysfs node accessibility across devices.

* CTI-Enable-Disable: Validates enable/disable operations on all detected CTI devices and ensures correct state transitions.
* CTI-Test: Verifies CTI trigger-in and trigger-out attach/detach functionality.
* Node-Access: Ensures all readable CoreSight sysfs nodes can be accessed without failure.

Signed-off-by: Rohan Dutta <rohadutt@qti.qualcomm.com>

Runner/suites/Kernel/DEBUG/CTI-Enable-Disable/run.sh
@@ -1,0 +1,152 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="CTI-Enable-Disable"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+cs_base="/sys/bus/coresight/devices"
+fail_count=0
+
+etf0_sink=""
+etr0_sink=""
+stm0_sink=""
+
+save_and_reset_devices() {
+    log_info "Saving state and resetting Coresight devices..."
+    if [ -f "$cs_base/tmc_etf0/enable_sink" ]; then
+        etf0_sink=$(cat "$cs_base/tmc_etf0/enable_sink" 2>/dev/null)
+        echo 0 > "$cs_base/tmc_etf0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -f "$cs_base/tmc_etr0/enable_sink" ]; then
+        etr0_sink=$(cat "$cs_base/tmc_etr0/enable_sink" 2>/dev/null)
+        echo 0 > "$cs_base/tmc_etr0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -f "$cs_base/stm0/enable_source" ]; then
+        stm0_sink=$(cat "$cs_base/stm0/enable_source" 2>/dev/null)
+        echo 0 > "$cs_base/stm0/enable_source" 2>/dev/null || true
+    fi
+}
+
+cleanup() {
+    log_info "Restoring Coresight devices state..."
+    if [ -n "$etf0_sink" ] && [ -f "$cs_base/tmc_etf0/enable_sink" ]; then
+        echo "$etf0_sink" > "$cs_base/tmc_etf0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -n "$etr0_sink" ] && [ -f "$cs_base/tmc_etr0/enable_sink" ]; then
+        echo "$etr0_sink" > "$cs_base/tmc_etr0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -n "$stm0_sink" ] && [ -f "$cs_base/stm0/enable_source" ]; then
+        echo "$stm0_sink" > "$cs_base/stm0/enable_source" 2>/dev/null || true
+    fi
+}
+
+trap cleanup EXIT
+
+if [ ! -d "$cs_base" ]; then
+    log_fail "Coresight directory not found: $cs_base"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+save_and_reset_devices
+
+if [ -f "$cs_base/tmc_etf0/enable_sink" ]; then
+    echo 1 > "$cs_base/tmc_etf0/enable_sink"
+else
+    log_warn "tmc_etf0 not found, proceeding without it..."
+fi
+
+cti_list=""
+for _dev in "$cs_base"/cti*; do
+    [ -e "$_dev" ] || continue
+    cti_list="$cti_list $(basename "$_dev")"
+done
+
+if [ -z "$cti_list" ]; then
+    log_fail "No CTI devices found."
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+else
+    for cti in $cti_list; do
+        dev_path="$cs_base/$cti"
+        
+        if [ ! -f "$dev_path/enable" ]; then
+            log_warn "Skipping $cti: 'enable' node not found"
+            continue
+        fi
+
+        log_info "Testing Device: $cti"
+
+        if ! echo 1 > "$dev_path/enable"; then
+            log_fail "$cti: Failed to write 1 to enable"
+            fail_count=$((fail_count + 1))
+            continue
+        fi
+
+        res=$(cat "$dev_path/enable")
+        if [ "$res" -eq 1 ]; then
+            log_pass "$cti Enabled Successfully"
+        else
+            log_fail "$cti Failed to Enable (Value: $res)"
+            fail_count=$((fail_count + 1))
+        fi
+
+        if ! echo 0 > "$dev_path/enable"; then
+            log_fail "$cti: Failed to write 0 to enable"
+            fail_count=$((fail_count + 1))
+            continue
+        fi
+
+        res=$(cat "$dev_path/enable")
+        if [ "$res" -eq 0 ]; then
+            log_pass "$cti Disabled Successfully"
+        else
+            log_fail "$cti Failed to Disable (Value: $res)"
+            fail_count=$((fail_count + 1))
+        fi
+    done
+fi
+
+if [ "$fail_count" -eq 0 ]; then
+    log_pass "CTI Enable/Disable Test Completed Successfully"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "CTI Enable/Disable Test Failed ($fail_count errors)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------"

Runner/suites/Kernel/DEBUG/Node-Access/run.sh
@@ -1,0 +1,118 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="Node-Access"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+cs_base="/sys/bus/coresight/devices"
+fail_count=0
+runs=3
+
+reset_source_sink() {
+    log_info "Dynamically finding and resetting all Coresight sources and sinks..."
+    for _dev in "$cs_base"/*; do
+        [ -e "$_dev" ] || continue
+        
+        # Disable if it's a source
+        if [ -f "$_dev/enable_source" ]; then
+            echo 0 > "$_dev/enable_source" 2>/dev/null || true
+        fi
+        
+        # Disable if it's a sink
+        if [ -f "$_dev/enable_sink" ]; then
+            echo 0 > "$_dev/enable_sink" 2>/dev/null || true
+        fi
+    done
+}
+
+if [ ! -d "$cs_base" ]; then
+    log_fail "Coresight directory $cs_base not found"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+set -- "$cs_base"/*
+if [ ! -e "$1" ]; then
+    log_fail "No Coresight devices found inside $cs_base"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+reset_source_sink
+
+i=0
+while [ "$i" -lt "$runs" ]; do
+    log_info "--- Iteration $((i+1)) / $runs ---"
+
+    for node_path in "$cs_base"/*; do
+        [ -d "$node_path" ] || continue
+
+        case "$(basename "$node_path")" in
+            *tpdm*)
+                continue
+                ;;
+        esac
+
+        for node in "$node_path"/*; do
+            if [ -f "$node" ] && [ -r "$node" ]; then
+                cat "$node" > /dev/null 2>&1 || true
+            fi
+        done
+
+        if [ -d "$node_path/mgmt" ]; then
+            for snode in "$node_path"/mgmt/*; do
+                if [ -f "$snode" ] && [ -r "$snode" ]; then
+                    if ! cat "$snode" > /dev/null 2>&1; then
+                        log_fail "Failed to read mgmt node: $snode"
+                        fail_count=$((fail_count + 1))
+                    fi
+                fi
+            done
+        fi
+    done
+    i=$((i+1))
+done
+
+if [ "$fail_count" -eq 0 ]; then
+    log_pass "All sysfs nodes Read Test PASS"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "Sysfs nodes Read Test FAIL ($fail_count errors)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------"

vnarapar/qcom-linux-test

Runner/suites/Kernel/DEBUG/CTI-Enable-Disable/run.sh
@@ -1,0 +1,152 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="CTI-Enable-Disable"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+CS_BASE="/sys/bus/coresight/devices"
+FAIL_COUNT=0
+
+ORIG_ETF0_SINK=""
+ORIG_ETR0_SINK=""
+ORIG_STM0_SOURCE=""
+
+save_and_reset_devices() {
+    log_info "Saving state and resetting Coresight devices..."
+    if [ -f "$CS_BASE/tmc_etf0/enable_sink" ]; then
+        ORIG_ETF0_SINK=$(cat "$CS_BASE/tmc_etf0/enable_sink" 2>/dev/null)
+        echo 0 > "$CS_BASE/tmc_etf0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -f "$CS_BASE/tmc_etr0/enable_sink" ]; then
+        ORIG_ETR0_SINK=$(cat "$CS_BASE/tmc_etr0/enable_sink" 2>/dev/null)
+        echo 0 > "$CS_BASE/tmc_etr0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -f "$CS_BASE/stm0/enable_source" ]; then
+        ORIG_STM0_SOURCE=$(cat "$CS_BASE/stm0/enable_source" 2>/dev/null)
+        echo 0 > "$CS_BASE/stm0/enable_source" 2>/dev/null || true
+    fi
+}
+
+cleanup() {
+    log_info "Restoring Coresight devices state..."
+    if [ -n "$ORIG_ETF0_SINK" ] && [ -f "$CS_BASE/tmc_etf0/enable_sink" ]; then
+        echo "$ORIG_ETF0_SINK" > "$CS_BASE/tmc_etf0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -n "$ORIG_ETR0_SINK" ] && [ -f "$CS_BASE/tmc_etr0/enable_sink" ]; then
+        echo "$ORIG_ETR0_SINK" > "$CS_BASE/tmc_etr0/enable_sink" 2>/dev/null || true
+    fi
+    if [ -n "$ORIG_STM0_SOURCE" ] && [ -f "$CS_BASE/stm0/enable_source" ]; then
+        echo "$ORIG_STM0_SOURCE" > "$CS_BASE/stm0/enable_source" 2>/dev/null || true
+    fi
+}
+
+trap cleanup EXIT
+
+if [ ! -d "$CS_BASE" ]; then
+    log_fail "Coresight directory not found: $CS_BASE"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+save_and_reset_devices
+
+if [ -f "$CS_BASE/tmc_etf0/enable_sink" ]; then
+    echo 1 > "$CS_BASE/tmc_etf0/enable_sink"
+else
+    log_warn "tmc_etf0 not found, proceeding without it..."
+fi
+
+CTI_LIST=""
+for _dev in "$CS_BASE"/cti*; do
+    [ -e "$_dev" ] || continue
+    CTI_LIST="$CTI_LIST $(basename "$_dev")"
+done
+
+if [ -z "$CTI_LIST" ]; then
+    log_fail "No CTI devices found."
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+else
+    for cti in $CTI_LIST; do
+        dev_path="$CS_BASE/$cti"
+        
+        if [ ! -f "$dev_path/enable" ]; then
+            log_warn "Skipping $cti: 'enable' node not found"
+            continue
+        fi
+
+        log_info "Testing Device: $cti"
+
+        if ! echo 1 > "$dev_path/enable"; then
+            log_fail "$cti: Failed to write 1 to enable"
+            FAIL_COUNT=$((FAIL_COUNT + 1))
+            continue
+        fi
+
+        res=$(cat "$dev_path/enable")
+        if [ "$res" -eq 1 ]; then
+            log_pass "$cti Enabled Successfully"
+        else
+            log_fail "$cti Failed to Enable (Value: $res)"
+            FAIL_COUNT=$((FAIL_COUNT + 1))
+        fi
+
+        if ! echo 0 > "$dev_path/enable"; then
+            log_fail "$cti: Failed to write 0 to enable"
+            FAIL_COUNT=$((FAIL_COUNT + 1))
+            continue
+        fi
+
+        res=$(cat "$dev_path/enable")
+        if [ "$res" -eq 0 ]; then
+            log_pass "$cti Disabled Successfully"
+        else
+            log_fail "$cti Failed to Disable (Value: $res)"
+            FAIL_COUNT=$((FAIL_COUNT + 1))
+        fi
+    done
+fi
+
+if [ "$FAIL_COUNT" -eq 0 ]; then
+    log_pass "CTI Enable/Disable Test Completed Successfully"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "CTI Enable/Disable Test Failed ($FAIL_COUNT errors)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------"

Runner/suites/Kernel/DEBUG/Node-Access/run.sh
@@ -1,0 +1,118 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="Node-Access"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+CS_BASE="/sys/bus/coresight/devices"
+FAIL_COUNT=0
+ITERATIONS=3
+
+reset_source_sink() {
+    log_info "Dynamically finding and resetting all Coresight sources and sinks..."
+    for _dev in "$CS_BASE"/*; do
+        [ -e "$_dev" ] || continue
+        
+        # Disable if it's a source
+        if [ -f "$_dev/enable_source" ]; then
+            echo 0 > "$_dev/enable_source" 2>/dev/null || true
+        fi
+        
+        # Disable if it's a sink
+        if [ -f "$_dev/enable_sink" ]; then
+            echo 0 > "$_dev/enable_sink" 2>/dev/null || true
+        fi
+    done
+}
+
+if [ ! -d "$CS_BASE" ]; then
+    log_fail "Coresight directory $CS_BASE not found"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+set -- "$CS_BASE"/*
+if [ ! -e "$1" ]; then
+    log_fail "No Coresight devices found inside $CS_BASE"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+reset_source_sink
+
+i=0
+while [ "$i" -lt "$ITERATIONS" ]; do
+    log_info "--- Iteration $((i+1)) / $ITERATIONS ---"
+
+    for node_path in "$CS_BASE"/*; do
+        [ -d "$node_path" ] || continue
+
+        case "$(basename "$node_path")" in
+            *tpdm*)
+                continue
+                ;;
+        esac
+
+        for node in "$node_path"/*; do
+            if [ -f "$node" ] && [ -r "$node" ]; then
+                cat "$node" > /dev/null 2>&1 || true
+            fi
+        done
+
+        if [ -d "$node_path/mgmt" ]; then
+            for snode in "$node_path"/mgmt/*; do
+                if [ -f "$snode" ] && [ -r "$snode" ]; then
+                    if ! cat "$snode" > /dev/null 2>&1; then
+                        log_fail "Failed to read mgmt node: $snode"
+                        FAIL_COUNT=$((FAIL_COUNT + 1))
+                    fi
+                fi
+            done
+        fi
+    done
+    i=$((i+1))
+done
+
+if [ "$FAIL_COUNT" -eq 0 ]; then
+    log_pass "All sysfs nodes Read Test PASS"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "Sysfs nodes Read Test FAIL ($FAIL_COUNT errors)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------"