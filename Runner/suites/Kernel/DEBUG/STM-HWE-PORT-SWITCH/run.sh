qualcomm-linux/qcom-linux-testkit

Added QDSS STM and TMC support in DEBUG suite
- MultiSource-STM-ETM: validates STM and ETM multi-source trace configuration
- Sink-Status-STM-Toggle: tests STM sink status and toggle functionality
- STM-HWE-PORT-SWITCH: verifies STM hardware event port switching
- STM-Source-Enable-Disable: tests STM source enable/disable via sysfs
- Added coresight_common.sh utility for shared functions

Signed-off-by: Rohan Dutta <rohadutt@qti.qualcomm.com>

Runner/suites/Kernel/DEBUG/STM-HWE-PORT-SWITCH/run.sh
@@ -1,0 +1,142 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="STM-HWE-PORT-SWITCH"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+rm -f "$res_file"
+touch "$res_file"
+
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+
+CS_BASE="/sys/bus/coresight/devices"
+STM_PATH="$CS_BASE/stm0"
+[ ! -d "$STM_PATH" ] && STM_PATH="$CS_BASE/coresight-stm"
+ETF_PATH="$CS_BASE/tmc_etf0"
+ETR_PATH="$CS_BASE/tmc_etr0"
+DEBUGFS="/sys/kernel/debug/tracing"
+
+reset_source_sink() {
+    if [ -f "$STM_PATH/enable_source" ]; then
+        echo 0 > "$STM_PATH/enable_source"
+    fi
+    if [ -f "$ETF_PATH/enable_sink" ]; then
+        echo 0 > "$ETF_PATH/enable_sink"
+    fi
+    if [ -f "$ETR_PATH/enable_sink" ]; then
+        echo 0 > "$ETR_PATH/enable_sink"
+    fi
+    if [ -f "$DEBUGFS/tracing_on" ]; then
+        echo 0 > "$DEBUGFS/tracing_on"
+    fi
+}
+
+test_attribute() {
+    attr_name=$1
+    attr_path="$STM_PATH/$attr_name"
+    
+    log_info "Testing Attribute: $attr_name"
+
+    if [ ! -f "$attr_path" ]; then
+        log_warn "Attribute $attr_name not found at $STM_PATH"
+        return 0
+    fi
+
+    for stm_state in 0 1; do
+        echo "$stm_state" > "$STM_PATH/enable_source"
+        
+        for val in 0 1; do
+            echo "$val" > "$attr_path"
+            readback=$(cat "$attr_path")
+            
+            
+            if [ "$attr_name" = "hwe_enable" ]; then
+                if [ "$readback" -eq "$val" ]; then
+                    log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                else
+                    log_fail "STM_Src:$stm_state | Failed to set $attr_name to $val (Read: $readback)"
+                    echo "$TESTNAME FAIL" > "$res_file"
+                    return 1
+                fi
+            elif [ "$attr_name" = "port_enable" ]; then
+                 if [ "$val" -eq 1 ] && [ "$readback" != "0" ] && [ "$readback" != "0x0" ]; then
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 elif [ "$val" -eq 0 ] && [ "$readback" = "0" ]; then 
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 elif [ "$val" -eq 0 ] && [ "$readback" = "0x0" ]; then
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 else
+                     log_fail "STM_Src:$stm_state | Failed to set $attr_name to $val (Read: $readback)"
+                     echo "$TESTNAME FAIL" > "$res_file"
+                     return 1
+                 fi
+            fi
+        done
+    done
+    
+    echo "$TESTNAME PASS" > "$res_file"
+    return 0
+}
+
+
+if [ ! -d "$STM_PATH" ]; then
+    log_fail "STM device not found"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+log_info "Creating Policy Directories..."
+mkdir -p /sys/kernel/config/stp-policy/stm0:p_ost.policy/default
+
+reset_source_sink
+
+if [ -f "$ETF_PATH/enable_sink" ]; then
+    echo 1 > "$ETF_PATH/enable_sink"
+fi
+
+test_attribute "hwe_enable"
+test_attribute "port_enable"
+
+reset_source_sink
+
+if [ -f "$STM_PATH/hwe_enable" ]; then
+    echo 0 > "$STM_PATH/hwe_enable"
+fi
+if [ -f "$STM_PATH/port_enable" ]; then
+    echo 0xffffffff > "$STM_PATH/port_enable"
+fi
+
+# log_info "-------------------$TESTNAME Testcase Finished----------------------------"

Runner/suites/Kernel/DEBUG/STM-Source-Enable-Disable/run.sh
@@ -1,0 +1,169 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="STM-Source-Enable-Disable"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+
+CS_BASE="/sys/bus/coresight/devices"
+FAIL_COUNT=0
+
+find_first_existing_path() {
+    for _dir_name in "$@"; do
+        if [ -d "$CS_BASE/$_dir_name" ]; then
+            echo "$CS_BASE/$_dir_name"
+            return 0
+        fi
+    done
+    echo ""
+}
+
+STM_PATH=$(find_first_existing_path "stm0" "coresight-stm")
+ETF_PATH=$(find_first_existing_path "tmc_etf0" "tmc_etf" "tmc_etf1")
+DEBUGFS="/sys/kernel/debug/tracing"
+
+reset_source_sink() {
+    for dev in "$CS_BASE"/*/; do
+        [ -d "$dev" ] || continue
+        if [ -f "$dev/enable_source" ]; then
+            val=$(cat "$dev/enable_source" 2>/dev/null)
+            if [ "$val" = "1" ]; then
+                echo 0 > "$dev/enable_source" 2>/dev/null
+                [ -f "$dev/reset" ] && echo 1 > "$dev/reset" 2>/dev/null
+            fi
+        fi
+        if [ -f "$dev/enable_sink" ]; then
+            val=$(cat "$dev/enable_sink" 2>/dev/null)
+            [ "$val" = "1" ] && echo 0 > "$dev/enable_sink" 2>/dev/null
+        fi
+    done
+}
+
+if [ -z "$STM_PATH" ]; then
+    log_fail "STM device not found in $CS_BASE"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+STM_NAME=$(basename "$STM_PATH")
+
+log_info "Setting up STP policy..."
+STP_POLICY_BASE="/sys/kernel/config/stp-policy"
+
+if ! grep -q "configfs" /proc/mounts 2>/dev/null; then
+    log_warn "configfs not mounted — skipping STP policy setup"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+if [ ! -d "$STP_POLICY_BASE" ]; then
+    log_warn "STP policy path not available: $STP_POLICY_BASE — skipping"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+if mkdir "$STP_POLICY_BASE/$STM_NAME:p_ost.policy" 2>/dev/null && \
+   mkdir "$STP_POLICY_BASE/$STM_NAME:p_ost.policy/default" 2>/dev/null; then
+    log_info "Using STP policy: p_ost"
+elif mkdir "$STP_POLICY_BASE/$STM_NAME:p_basic.policy" 2>/dev/null && \
+     mkdir "$STP_POLICY_BASE/$STM_NAME:p_basic.policy/default" 2>/dev/null; then
+    log_info "Using STP policy: p_basic"
+elif [ -d "$STP_POLICY_BASE/$STM_NAME:p_basic.policy/default" ]; then
+    log_info "Using existing STP policy: p_basic"
+elif [ -d "$STP_POLICY_BASE/$STM_NAME:p_ost.policy/default" ]; then
+    log_info "Using existing STP policy: p_ost"
+else
+    log_warn "No STP policy could be created — skipping"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+log_info "Initial cleanup..."
+reset_source_sink
+
+if [ -f "$STM_PATH/hwevent_enable" ]; then
+    echo 0 > "$STM_PATH/hwevent_enable"
+fi
+if [ -f "$STM_PATH/port_enable" ]; then
+    echo 0xffffffff > "$STM_PATH/port_enable"
+fi
+
+if ! grep -q "debugfs" /proc/mounts 2>/dev/null; then
+    log_warn "debugfs not mounted — skipping events/enable reset"
+elif [ -f "$DEBUGFS/events/enable" ]; then
+    echo 0 > "$DEBUGFS/events/enable"
+else
+    log_warn "$DEBUGFS/events/enable not found — skipping"
+fi
+
+log_info "Starting 50 iteration loop..."
+
+if [ -n "$ETF_PATH" ] && [ -f "$ETF_PATH/enable_sink" ]; then
+    echo 1 > "$ETF_PATH/enable_sink" 2>/dev/null
+fi
+
+_count=1
+while [ "$_count" -le 50 ]; do
+    
+    echo 1 > "$STM_PATH/enable_source" 2>/dev/null
+    val=$(cat "$STM_PATH/enable_source" 2>/dev/null)
+    
+    if [ "$val" != "1" ]; then
+        log_fail "Iteration $_count: Failed to enable STM source"
+        FAIL_COUNT=$((FAIL_COUNT + 1))
+    fi
+
+    echo 0 > "$STM_PATH/enable_source" 2>/dev/null
+    val=$(cat "$STM_PATH/enable_source" 2>/dev/null)
+    
+    if [ "$val" != "0" ]; then
+        log_fail "Iteration $_count: Failed to disable STM source"
+        FAIL_COUNT=$((FAIL_COUNT + 1))
+    fi
+
+    _count=$((_count + 1))
+done
+
+reset_source_sink
+
+if [ "$FAIL_COUNT" -eq 0 ]; then
+    log_pass "STM source enable/disable loop passed (50 iterations)"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "STM source enable/disable loop failed ($FAIL_COUNT failures)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi

vnarapar/qcom-linux-test

Runner/suites/Kernel/DEBUG/STM-HWE-PORT-SWITCH/run.sh
@@ -1,0 +1,142 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="STM-HWE-PORT-SWITCH"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+rm -f "$res_file"
+touch "$res_file"
+
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+
+cs_base="/sys/bus/coresight/devices"
+stm_path="$cs_base/stm0"
+[ ! -d "$stm_path" ] && stm_path="$cs_base/coresight-stm"
+etf_path="$cs_base/tmc_etf0"
+etr_path="$cs_base/tmc_etr0"
+debugfs="/sys/kernel/debug/tracing"
+
+reset_source_sink() {
+    if [ -f "$stm_path/enable_source" ]; then
+        echo 0 > "$stm_path/enable_source"
+    fi
+    if [ -f "$etf_path/enable_sink" ]; then
+        echo 0 > "$etf_path/enable_sink"
+    fi
+    if [ -f "$etr_path/enable_sink" ]; then
+        echo 0 > "$etr_path/enable_sink"
+    fi
+    if [ -f "$debugfs/tracing_on" ]; then
+        echo 0 > "$debugfs/tracing_on"
+    fi
+}
+
+test_attribute() {
+    attr_name=$1
+    attr_path="$stm_path/$attr_name"
+    
+    log_info "Testing Attribute: $attr_name"
+
+    if [ ! -f "$attr_path" ]; then
+        log_warn "Attribute $attr_name not found at $stm_path"
+        return 0
+    fi
+
+    for stm_state in 0 1; do
+        echo "$stm_state" > "$stm_path/enable_source"
+        
+        for val in 0 1; do
+            echo "$val" > "$attr_path"
+            readback=$(cat "$attr_path")
+            
+            
+            if [ "$attr_name" = "hwe_enable" ]; then
+                if [ "$readback" -eq "$val" ]; then
+                    log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                else
+                    log_fail "STM_Src:$stm_state | Failed to set $attr_name to $val (Read: $readback)"
+                    echo "$TESTNAME FAIL" > "$res_file"
+                    return 1
+                fi
+            elif [ "$attr_name" = "port_enable" ]; then
+                 if [ "$val" -eq 1 ] && [ "$readback" != "0" ] && [ "$readback" != "0x0" ]; then
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 elif [ "$val" -eq 0 ] && [ "$readback" = "0" ]; then 
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 elif [ "$val" -eq 0 ] && [ "$readback" = "0x0" ]; then
+                     log_pass "STM_Src:$stm_state | $attr_name set to $val"
+                 else
+                     log_fail "STM_Src:$stm_state | Failed to set $attr_name to $val (Read: $readback)"
+                     echo "$TESTNAME FAIL" > "$res_file"
+                     return 1
+                 fi
+            fi
+        done
+    done
+    
+    echo "$TESTNAME PASS" > "$res_file"
+    return 0
+}
+
+
+if [ ! -d "$stm_path" ]; then
+    log_fail "STM device not found"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+log_info "Creating Policy Directories..."
+mkdir -p /sys/kernel/config/stp-policy/stm0:p_ost.policy/default
+
+reset_source_sink
+
+if [ -f "$etf_path/enable_sink" ]; then
+    echo 1 > "$etf_path/enable_sink"
+fi
+
+test_attribute "hwe_enable"
+test_attribute "port_enable"
+
+reset_source_sink
+
+if [ -f "$stm_path/hwe_enable" ]; then
+    echo 0 > "$stm_path/hwe_enable"
+fi
+if [ -f "$stm_path/port_enable" ]; then
+    echo 0xffffffff > "$stm_path/port_enable"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------" 

Runner/suites/Kernel/DEBUG/STM-Source-Enable-Disable/run.sh
@@ -1,0 +1,171 @@
+#!/bin/sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+INIT_ENV=""
+SEARCH="$SCRIPT_DIR"
+while [ "$SEARCH" != "/" ]; do
+    if [ -f "$SEARCH/init_env" ]; then
+        INIT_ENV="$SEARCH/init_env"
+        break
+    fi
+    SEARCH=$(dirname "$SEARCH")
+done
+
+if [ -z "$INIT_ENV" ]; then
+    echo "[ERROR] Could not find init_env" >&2
+    exit 1
+fi
+
+if [ -z "$__INIT_ENV_LOADED" ]; then
+    # shellcheck disable=SC1090
+    . "$INIT_ENV"
+fi
+
+# shellcheck disable=SC1090,SC1091
+. "$TOOLS/functestlib.sh"
+
+TESTNAME="STM-Source-Enable-Disable"
+if command -v find_test_case_by_name >/dev/null 2>&1; then
+    test_path=$(find_test_case_by_name "$TESTNAME")
+    cd "$test_path" || exit 1
+else
+    cd "$SCRIPT_DIR" || exit 1
+fi
+
+res_file="./$TESTNAME.res"
+log_info "-----------------------------------------------------------------------------------------"
+log_info "-------------------Starting $TESTNAME Testcase----------------------------"
+
+cs_base="/sys/bus/coresight/devices"
+fail=0
+
+find_first_existing_path() {
+    for _dir_name in "$@"; do
+        if [ -d "$cs_base/$_dir_name" ]; then
+            echo "$cs_base/$_dir_name"
+            return 0
+        fi
+    done
+    echo ""
+}
+
+stm_path=$(find_first_existing_path "stm0" "coresight-stm")
+etf_path=$(find_first_existing_path "tmc_etf0" "tmc_etf" "tmc_etf1")
+debugfs="/sys/kernel/debug/tracing"
+
+reset_source_sink() {
+    for dev in "$cs_base"/*/; do
+        [ -d "$dev" ] || continue
+        if [ -f "$dev/enable_source" ]; then
+            val=$(cat "$dev/enable_source" 2>/dev/null)
+            if [ "$val" = "1" ]; then
+                echo 0 > "$dev/enable_source" 2>/dev/null
+                [ -f "$dev/reset" ] && echo 1 > "$dev/reset" 2>/dev/null
+            fi
+        fi
+        if [ -f "$dev/enable_sink" ]; then
+            val=$(cat "$dev/enable_sink" 2>/dev/null)
+            [ "$val" = "1" ] && echo 0 > "$dev/enable_sink" 2>/dev/null
+        fi
+    done
+}
+
+if [ -z "$stm_path" ]; then
+    log_fail "STM device not found in $cs_base"
+    echo "$TESTNAME FAIL" > "$res_file"
+    exit 1
+fi
+
+stm_name=$(basename "$stm_path")
+
+log_info "Setting up STP policy..."
+stp_policy_base="/sys/kernel/config/stp-policy"
+
+if ! grep -q "configfs" /proc/mounts 2>/dev/null; then
+    log_warn "configfs not mounted — skipping STP policy setup"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+if [ ! -d "$stp_policy_base" ]; then
+    log_warn "STP policy path not available: $stp_policy_base — skipping"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+if mkdir "$stp_policy_base/$stm_name:p_ost.policy" 2>/dev/null && \
+   mkdir "$stp_policy_base/$stm_name:p_ost.policy/default" 2>/dev/null; then
+    log_info "Using STP policy: p_ost"
+elif mkdir "$stp_policy_base/$stm_name:p_basic.policy" 2>/dev/null && \
+     mkdir "$stp_policy_base/$stm_name:p_basic.policy/default" 2>/dev/null; then
+    log_info "Using STP policy: p_basic"
+elif [ -d "$stp_policy_base/$stm_name:p_basic.policy/default" ]; then
+    log_info "Using existing STP policy: p_basic"
+elif [ -d "$stp_policy_base/$stm_name:p_ost.policy/default" ]; then
+    log_info "Using existing STP policy: p_ost"
+else
+    log_warn "No STP policy could be created — skipping"
+    echo "$TESTNAME Skip" > "$res_file"
+    exit 0
+fi
+
+log_info "Initial cleanup..."
+reset_source_sink
+
+if [ -f "$stm_path/hwevent_enable" ]; then
+    echo 0 > "$stm_path/hwevent_enable"
+fi
+if [ -f "$stm_path/port_enable" ]; then
+    echo 0xffffffff > "$stm_path/port_enable"
+fi
+
+if ! grep -q "debugfs" /proc/mounts 2>/dev/null; then
+    log_warn "debugfs not mounted — skipping events/enable reset"
+elif [ -f "$debugfs/events/enable" ]; then
+    echo 0 > "$debugfs/events/enable"
+else
+    log_warn "$debugfs/events/enable not found — skipping"
+fi
+
+log_info "Starting 50 iteration loop..."
+
+if [ -n "$etf_path" ] && [ -f "$etf_path/enable_sink" ]; then
+    echo 1 > "$etf_path/enable_sink" 2>/dev/null
+fi
+
+count=1
+while [ "$count" -le 50 ]; do
+    
+    echo 1 > "$stm_path/enable_source" 2>/dev/null
+    val=$(cat "$stm_path/enable_source" 2>/dev/null)
+    
+    if [ "$val" != "1" ]; then
+        log_fail "Iteration $count: Failed to enable STM source"
+        fail=$((fail + 1))
+    fi
+
+    echo 0 > "$stm_path/enable_source" 2>/dev/null
+    val=$(cat "$stm_path/enable_source" 2>/dev/null)
+    
+    if [ "$val" != "0" ]; then
+        log_fail "Iteration $count: Failed to disable STM source"
+        fail=$((fail + 1))
+    fi
+
+    count=$((count + 1))
+done
+
+reset_source_sink
+
+if [ "$fail" -eq 0 ]; then
+    log_pass "STM source enable/disable loop passed (50 iterations)"
+    echo "$TESTNAME PASS" > "$res_file"
+else
+    log_fail "STM source enable/disable loop failed ($fail failures)"
+    echo "$TESTNAME FAIL" > "$res_file"
+fi
+
+log_info "-------------------$TESTNAME Testcase Finished----------------------------" 